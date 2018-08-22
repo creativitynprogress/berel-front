8ab849a6dc5b12dce2b7c7fb63171cfa2116207a
Gerson Flores Acuña <gerson71fa@hotmail.com>
2018-08-22 16:41:21 -0500

    fix subsidiary

--- create-installer.js
+++ create-installer.js
@@ -2,10 +2,10 @@ var electronInstaller = require('electron-winstaller');
 const path = require('path')
 
 resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: '/electron/b-code',
-    outputDirectory: '/tmp/build/b-code',
+    appDirectory: './BCODECOLORS-win32-ia32',
+    outputDirectory: '/BCODECOLORS-installer',
     authors: 'Creativity & Progress',
-    exe: 'colors.exe'
+    setupExe: 'bcodecolors.exe'
   });
 
 resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
