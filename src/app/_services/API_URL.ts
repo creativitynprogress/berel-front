b6dcb1d0adab50a43794fe8240bbbb8bee6bd960
Gerson Flores Acuña <gerson71fa@hotmail.com>
2018-08-27 21:56:33 -0500

    fix url

--- src/app/_services/API_URL.ts
+++ src/app/_services/API_URL.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3020';
-//export const API_URL = 'http://bcodecolors.com';
+//export const API_URL = 'http://localhost:3020';
+export const API_URL = 'http://bcodecolors.com';
 //export const API_URL = 'http://45.33.55.120/berel';
